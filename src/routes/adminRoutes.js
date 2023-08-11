f5a6f855a01a6f83686ffb785ea51ae76f2ebe6f
Xavier Maillou <axrevi@gmail.com>
2023-08-11 23:13:47 +0200

    axios added + routes to handle data redirected to data server

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,80 +1,43 @@
 const fs = require('fs')
+const axios = require('axios')
 const { readJSON } = require('../lib/common')
 
 module.exports = (app) => {
-    app.get('/kanjiFullList', (req, res) => {
-        readJSON('kanji', (data) => res.json(data))
+    app.get('/kanjiFullList', async (req, res) => {
+        const result = await axios.get("http://localhost:9002/kanji")
+        res.json(result.data)
     })
-    app.get('/vocabularyFullList', (req, res) => {
-        readJSON('vocabulary', (data) => res.json(data))
+    app.get('/vocabularyFullList', async (req, res) => {
+        const result = await axios.get("http://localhost:9002/vocabulary")
+        res.json(result.data)
     })
-    app.get('/sentencesFullList', (req, res) => {
-        readJSON('sentences', (data) => res.json(data))
-    })
-
-    const addElement = (type, body, data, res) => {
-        body.id = data.length + 1
-
-        const updatedData = [ body, ...data ]
-
-        fs.writeFile(process.cwd() + `/src/data/${type}.json`, JSON.stringify(updatedData), (err) => {
-            if (err) {
-                console.log("An error has occurred ", err)
-                return
-            }
-            console.log("Data written successfully to the file:", type)
-            console.log("Element added:", body)
-            res.sendStatus(204)
-        })
-    }
-
-    const editElement = (type, id, body, data, res) => {
-        const updatedData = data.map((element) => {
-            if (Number(element.id) === id) return body
-            else return element
-        })
-
-        fs.writeFile(process.cwd() + `/src/data/${type}.json`, JSON.stringify(updatedData), (err) => {
-            if (err) {
-                console.log("An error has occurred ", err)
-                return
-            }
-            console.log("Data written successfully to the file:", type)
-            console.log("Element edited:", body)
-            res.sendStatus(204)
-        })
-    }
-
-    app.post('/kanji', (req, res) => {
-        const body = req.body
-        readJSON('kanji', (data) => addElement('kanji', body, data, res))
+    app.get('/sentencesFullList', async (req, res) => {
+        const result = await axios.get("http://localhost:9002/sentences")
+        res.json(result.data)
     })
 
-    app.post('/kanji/:id', (req, res) => {
-        const id = Number(req.params.id)
-        const body = req.body
-        readJSON('kanji', (data) => editElement('kanji', id, body, data, res))
+    app.post('/kanji', async (req, res) => {
+        const result = await axios.post("http://localhost:9002/kanji", req.body)
+        res.json(result.data)
     })
-
-    app.post('/word', (req, res) => {
-        const body = req.body
-        readJSON('vocabulary', (data) => addElement('vocabulary', body, data, res))
+    app.post('/kanji/:id', async (req, res) => {
+        const result = await axios.put(`http://localhost:9002/kanji/${req.params.id}`, req.body)
+        res.json(result.data)
     })
-
-    app.post('/word/:id', (req, res) => {
-        const id = Number(req.params.id)
-        const body = req.body
-        readJSON('vocabulary', (data) => editElement('vocabulary', id, body, data, res))
+    app.post('/word', async (req, res) => {
+        const result = await axios.post("http://localhost:9002/vocabulary", req.body)
+        res.json(result.data)
     })
-
-    app.post('/sentence', (req, res) => {
-        const body = req.body
-        readJSON('sentences', (data) => addElement('sentences', body, data, res))
+    app.post('/word/:id', async (req, res) => {
+        const result = await axios.put(`http://localhost:9002/vocabulary/${req.params.id}`, req.body)
+        res.json(result.data)
     })
-
-    app.post('/sentence/:id', (req, res) => {
-        const id = Number(req.params.id)
-        const body = req.body
-        readJSON('sentences', (data) => editElement('sentences', id, body, data, res))
+    app.post('/sentence', async (req, res) => {
+        const result = await axios.post("http://localhost:9002/sentences", req.body)
+        res.json(result.data)
+    })
+    app.post('/sentence/:id', async (req, res) => {
+        const result = await axios.put(`http://localhost:9002/sentences/${req.params.id}`, req.body)
+        res.json(result.data)
     })
 }
