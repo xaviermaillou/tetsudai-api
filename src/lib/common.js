5b7b777c20c184ee2c3e5333e4234a38dbaf3a0d
Xavier Maillou <axrevi@gmail.com>
2023-09-10 18:00:52 +0200

    fix

--- src/lib/common.js
+++ src/lib/common.js
@@ -53,8 +53,9 @@ module.exports = {
     findComposingWords: (stringsArray, string) => {
         // We create a copy of 'search' string, which will be sliced from the beginning at each found word
         let searchCopy = string
-        let foundWords = []
-
+        let alternativeSearchCopy = string
+        const foundWords = []
+        const alternativeFoundWords = []
         for (let i = 0; i < searchCopy.length; i++) {
             // 'stringToCompare' value is equal to 'searchCopy' with an 'i' amount of letters removed from its ending
             const stringToCompare = i === 0 ? searchCopy : searchCopy.slice(0, -i)
@@ -80,26 +81,24 @@ module.exports = {
         }
         // If an element has been skipped, we redo the loop in reverse
         if (foundWords.join("").length < string.length) {
-            searchCopy = string
-            foundWords = []
-            for (let i = 0; i < searchCopy.length; i++) {
-                const stringToCompare = i === 0 ? searchCopy : searchCopy.slice(-i)
+            for (let i = 0; i < alternativeSearchCopy.length; i++) {
+                const stringToCompare = i === 0 ? alternativeSearchCopy : alternativeSearchCopy.slice(-i)
                 if (stringsArray.includes(stringToCompare)) {
-                    foundWords.unshift(stringToCompare)
-                    searchCopy = searchCopy.slice(0, -i)
-                    if (searchCopy === stringToCompare) break
+                    alternativeFoundWords.unshift(stringToCompare)
+                    alternativeSearchCopy = alternativeSearchCopy.slice(0, -i)
+                    if (alternativeSearchCopy === stringToCompare) break
                     else i = -1
                 }
-                else if (i === (searchCopy.length - 1)) {
+                else if (i === (alternativeSearchCopy.length - 1)) {
                     if (sentenceExceptionCharacters.includes(stringToCompare)) {
-                        foundWords.unshift(stringToCompare)
+                        alternativeFoundWords.unshift(stringToCompare)
                     }
-                    searchCopy = searchCopy.slice(0, -i)
-                    if (searchCopy === stringToCompare) break
+                    alternativeSearchCopy = alternativeSearchCopy.slice(0, -i)
+                    if (alternativeSearchCopy === stringToCompare) break
                     else i = -1
                 }
             }
         }
-        return foundWords
+        return foundWords.join("").length > alternativeFoundWords.join("").length ? foundWords : alternativeFoundWords
     }
 }
